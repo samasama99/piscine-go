dd3cfa618e9f532a9dee862d20f1fcfc6b8be72a
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-02 15:32:46 +0100

    quest 04 -- second push

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,4 +1,1 @@
-DATA=$(curl -s https://oujda.01-edu.org/assets/superhero/all.json)
-
-
-echo $DATA | jq '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' | tr -d "\""
+curl -s https://oujda.01-edu.org/assets/superhero/all.json | jq '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender' | tr -d "\""
