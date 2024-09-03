f3a1aa23ac93e8f7d903bef2e89aca9c4e0cfba7
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-03 17:52:05 +0100

    strrev push 1

---
+++ strrev.go
@@ -0,0 +1,13 @@
+package piscine
+
+func StrRev(s string) string {
+	sliceOfRunes := []rune(s)
+	var rev = make([]rune, len(s))
+	length := len(sliceOfRunes)
+
+	for i, value := range sliceOfRunes {
+		rev[length-i-1] = value
+	}
+
+	return string(rev)
+}
