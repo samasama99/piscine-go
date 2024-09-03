555e2cb3eb5ca4575e048751797e33e138d32f95
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-03 13:08:50 +0100

    go00 - sixth push

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,9 +4,7 @@ import "github.com/01-edu/z01"
 
 func main() {
 	for i := 'a'; i < 'z'+1; i++ {
-		err := z01.PrintRune(i)
-		if err != nil {
-			return
-		}
+		z01.PrintRune(i)
 	}
+	z01.PrintRune('\n')
 }
