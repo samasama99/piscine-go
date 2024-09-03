2d309aac30b7dd525fe970a1f95e21d32ae047d5
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-03 16:48:34 +0100

    format

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module piscine
 
 go 1.22.6
+
+require github.com/01-edu/z01 v0.1.0
--- strlen.go
+++ strlen.go
@@ -1,9 +1,6 @@
 package piscine
 
 func StrLen(s string) int {
-  	res := 0
-	for i, _ := range []rune(s) {
-		res = i + 1
-	}
-    return res
+	sliceOfRunes := []rune(s)
+	return len(sliceOfRunes)
 }
