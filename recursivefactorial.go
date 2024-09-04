24ec366090796e4b0f6fa1b88551782051c85d6f
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 12:40:18 +0100

    quest 04 01 push 2

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,19 @@
 package piscine
 
-func RecursiveFactorial(nb int) int {
+func recursiveFactorialHelper(nb, current int) int {
 	if nb == 0 {
 		return 1
 	}
+	tmp := current * nb
+	if tmp < current {
+		return 0
+	}
+	return recursiveFactorialHelper(nb-1, tmp)
+}
+
+func RecursiveFactorial(nb int) int {
 	if nb < 0 {
 		return 0
 	}
-	return nb * RecursiveFactorial(nb-1)
+	return recursiveFactorialHelper(nb, 1)
 }
