a3fb2cf8bb0493fa6716ba42562a875f9eb3c95c
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 11:33:35 +0100

    basic atoi 2 push 1

---
+++ basicatoi2.go
@@ -0,0 +1,17 @@
+package piscine
+
+func isValidNumber(s string) bool {
+	for _, c := range s {
+		if c < '0' || c > '9' {
+			return false
+		}
+	}
+	return true
+}
+
+func BasicAtoi2(s string) int {
+	if isValidNumber(s) {
+		return 0
+	}
+	return BasicAtoi(s)
+}
