e0db4c11a8872de329003baaa38a0b822d0c85c3
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 14:48:29 +0100

    quest 05 10 push 3

--- isalpha.go
+++ isalpha.go
@@ -1,5 +1,20 @@
 package piscine
 
+func isNumeric(c rune) bool {
+	return c >= '0' && c <= '9'
+}
+
+func alphaNumericCount(s string) int {
+	runes := []rune(s)
+	count := 0
+	for _, r := range runes {
+		if isAlpha(r) || isNumeric(r) {
+			count++
+		}
+	}
+	return count
+}
+
 func IsAlpha(s string) bool {
-	return AlphaCount(s) == len(s)
+	return alphaNumericCount(s) == len(s)
 }
