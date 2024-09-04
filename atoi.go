c84693f0d0e4fb2fe3c295deed690d19eb413dde
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 12:25:07 +0100

    format

--- atoi.go
+++ atoi.go
@@ -34,10 +34,10 @@ func BasicAtoi2(s string) int {
 }
 
 func Atoi(s string) int {
-    if len(s) == 0 {
-      return 0
-    }
-  
+	if len(s) == 0 {
+		return 0
+	}
+
 	if s == "-9223372036854775808" {
 		return -9223372036854775808
 	}
@@ -45,7 +45,7 @@ func Atoi(s string) int {
 	if s[0] == '-' {
 		return BasicAtoi2(s[1:]) * -1
 	} else if s[0] == '+' {
-        return BasicAtoi2(s[1:])
-    }
+		return BasicAtoi2(s[1:])
+	}
 	return BasicAtoi2(s)
 }
