11f89bdfaf047fe0bc7211b9183e554e7becd7bc
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 16:44:45 +0100

    quest 05 16 push 3

--- trimatoi.go
+++ trimatoi.go
@@ -4,6 +4,9 @@ func TrimAtoi(s string) int {
 	newString := make([]rune, len(s))
 	index := 0
 	for _, c := range s {
+		if index == 0 && c == '-' {
+			newString[index] = '-'
+		}
 		if isNumeric(c) {
 			newString[index] = c
 			index++
