accae7706c69a92e8bcfaf46c53f6821723c4c27
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-04 11:19:40 +0100

    basic atoi push 2

--- basicatoi.go
+++ basicatoi.go
@@ -10,8 +10,9 @@ func pow(num, power int) int {
 
 func BasicAtoi(s string) int {
 	res := 0
+	length := len(s)
 	for i, value := range s {
-		res += (int(value) - 48) * pow(10, i)
+		res += (int(value) - 48) * pow(10, length-i-1)
 	}
 	return res
 }
