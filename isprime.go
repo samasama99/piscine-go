41e29186c3baa25a0facd18434f085dcb100a147
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-05 16:00:53 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -1,12 +1,10 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb == 0 {
+	if nb <= 0 {
 		return false
 	}
-	if nb < 0 {
-		nb = nb * -1
-	}
+
 	index := 2
 	for index*index <= nb {
 		if nb%index == 0 {
