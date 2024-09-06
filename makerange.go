6c41a8c6f76b5545bd2bef22fb27a87c4cbbd840
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-06 12:16:26 +0100

    make range

--- makerange.go
+++ makerange.go
@@ -2,11 +2,11 @@ package piscine
 
 func MakeRange(min, max int) []int {
 	length := max - min
+	if length < 1 {
+		return []int{}
+	}
 	arr := make([]int, length)
 	index := 0
-	if length < 1 {
-		return arr
-	}
 	for i := min; i < max; i++ {
 		arr[index] = i
 		index++
