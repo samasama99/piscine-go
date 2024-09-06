904173c8d379eb547fc518b5f731aac7437549de
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-06 14:25:52 +0100

    split

---
+++ split.go
@@ -0,0 +1,22 @@
+package piscine
+
+func Split(s, sep string) []string {
+	var result []string
+	var current string
+
+	sliceLen := len(sep)
+	for i := 0; i < len(s)-sliceLen; i++ {
+		slice := s[i : i+sliceLen]
+		if slice == sep && len(current) > 0 {
+			result = append(result, current)
+			current = ""
+		} else if slice != sep {
+			current += string(s[i])
+		}
+	}
+
+	if len(current) > 0 {
+		result = append(result, current)
+	}
+	return result
+}
