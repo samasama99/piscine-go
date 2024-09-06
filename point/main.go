063cebda39fec7a0125c7d5cc57c284c42a74339
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-06 16:26:17 +0100

    point

--- point/main.go
+++ point/main.go
@@ -33,11 +33,31 @@ func intToString(n int) string {
 	if n == 0 {
 		z01.PrintRune(rune(48))
 	}
-
 	tmp := n
 	res := ""
 	for tmp != 0 {
-		res = string(rune(tmp%10+48)) + res
+		switch tmp % 10 {
+		case 0:
+			res = "0" + res
+		case 1:
+			res = "1" + res
+		case 2:
+			res = "2" + res
+		case 3:
+			res = "3" + res
+		case 4:
+			res = "4" + res
+		case 5:
+			res = "5" + res
+		case 6:
+			res = "6" + res
+		case 7:
+			res = "7" + res
+		case 8:
+			res = "8" + res
+		case 9:
+			res = "9" + res
+		}
 		tmp = tmp / 10
 	}
 
