39426561ca8ee4e0d56beaa6e394675c35127e84
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-06 11:49:12 +0100

    range

---
+++ appendrange.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	length := max - min
+	if length < 1 {
+		return []int{}
+	}
+	arr := make([]int, length)
+	index := 0
+	for i := min; i < max; i++ {
+		arr[index] = i
+		index++
+	}
+	return arr
+}
