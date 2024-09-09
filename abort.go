9b98c5a62d4dc81fefd0fb1b46fcdd3950650584
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 16:33:30 +0100

    abort.go

--- abort.go
+++ abort.go
@@ -3,7 +3,7 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 	arr := []int{a, b, c, d, e}
 	limit := 5
-	for i := 1; i < limit; i++ {
+	for i := 1; i < 5; i++ {
 		for j := 1; j < limit; j++ {
 			if arr[j-1] > arr[j] {
 				arr[j], arr[j-1] = arr[j-1], arr[j]
