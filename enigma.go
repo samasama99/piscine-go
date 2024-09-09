9c52d4fa1e7fd3091fcbe01a1b8e88580ae69332
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 17:56:51 +0100

    enigma.go

---
+++ enigma.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tmpA := ***a
+	tmpB := *b
+	tmpC := *******c
+	tmpD := ****d
+
+	***a = tmpC
+	*b = tmpA
+	*******c = tmpD
+	****d = tmpB
+}
