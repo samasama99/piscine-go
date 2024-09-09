f7ee7153f779f97f4bd1b492c3cefc4ae9507e91
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 13:00:36 +0100

    issorted.go

--- issorted.go
+++ issorted.go
@@ -4,13 +4,24 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	if len(a) < 2 {
 		return true
 	}
+
 	_a := a[0]
 	remain := a[1:]
+	sorted := 0
+
 	for _, _b := range remain {
-		if f(_a, _b) > 0 {
-			return false
+		if sorted == 0 {
+			if f(_a, _b) < 0 {
+				sorted = -1
+			} else if f(_a, _b) > 0 {
+				sorted = 1
+			}
+		} else {
+			if sorted != f(_a, _b) {
+				return false
+			}
 		}
 		_a = _b
 	}
-	return true
+	return false
 }
