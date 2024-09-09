8a226e3278b0fe28873150be1408586ca36472c0
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 12:37:08 +0100

    issorted.go

---
+++ issorted.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	if len(a) < 2 {
+		return true
+	}
+	_a := a[0]
+	remain := a[1:]
+	for _, _b := range remain {
+		if f(_a, _b) >= 0 {
+			return false
+		}
+		_a = _b
+	}
+	return true
+}
