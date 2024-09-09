34513a1b102aa9bee505b4e87d25b2075bb13a78
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 15:07:20 +0100

    rot14.go

--- rot14.go
+++ rot14.go
@@ -4,15 +4,15 @@ func Rot14(s string) string {
 	bytes := make([]rune, len(s))
 	copy(bytes, []rune(s))
 	for i, b := range bytes {
-		if isAlpha(b) {
-			if isUpper(b) {
+		if _isAlpha(b) {
+			if _isUpper(b) {
 				if b+14 > 'Z' {
 					bytes[i] = 'A' + (b + 14 - 'Z')
 				} else {
 					bytes[i] = b + 14
 				}
 			}
-			if isLower(b) {
+			if _isLower(b) {
 				if b+14 > 'z' {
 					bytes[i] = 'a' + (b + 14 - 'z')
 				} else {
@@ -22,4 +22,14 @@ func Rot14(s string) string {
 		}
 	}
 	return string(bytes)
+}
+
+func _isAlpha(c rune) bool {
+	return c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z'
+}
+func _isUpper(r rune) bool {
+	return r >= 'A' && r <= 'Z'
+}
+func _isLower(r rune) bool {
+	return r >= 'a' && r <= 'z'
 }
