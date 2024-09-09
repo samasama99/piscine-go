3533f4b387c96056dc9e39defd940456260f7489
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 15:12:04 +0100

    rot14.go

--- rot14.go
+++ rot14.go
@@ -7,14 +7,14 @@ func Rot14(s string) string {
 		if _isAlpha(b) {
 			if _isUpper(b) {
 				if b+14 > 'Z' {
-					bytes[i] = 'A' + (b + 14 - 'Z')
+					bytes[i] = ('A' - 1) + (b + 14 - 'Z')
 				} else {
 					bytes[i] = b + 14
 				}
 			}
 			if _isLower(b) {
 				if b+14 > 'z' {
-					bytes[i] = 'a' + (b + 14 - 'z')
+					bytes[i] = ('a' - 1) + (b + 14 - 'z')
 				} else {
 					bytes[i] = b + 14
 				}
