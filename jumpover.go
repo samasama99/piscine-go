01760ed5544c2a4b9085233bdd40b8383665da61
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 17:41:21 +0100

    jumpover.go

--- jumpover.go
+++ jumpover.go
@@ -4,11 +4,5 @@ func JumpOver(str string) string {
 	if len(str) <= 2 {
 		return "\n"
 	}
-	runes := make([]rune, 0)
-	for i, c := range str {
-		if i != 0 && i%2 == 0 {
-			runes = append(runes, c)
-		}
-	}
-	return string(runes)
+	return str[2:3] + JumpOver(str[3:])
 }
