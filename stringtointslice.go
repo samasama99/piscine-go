5d9503bdc32ba5d05a60dc9db6f12f3b29abf6dc
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 17:50:36 +0100

    stringtointslice.go

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,6 +1,9 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
+	if len(str) == 0 {
+		return []int(nil)
+	}
 	runes := []rune(str)
 	IntSlice := make([]int, len(runes))
 	for i, r := range runes {
