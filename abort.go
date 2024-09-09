551cddbbdb26e08a0ae0c208fe7289be61dbc027
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 16:27:11 +0100

    abort.go

--- abort.go
+++ abort.go
@@ -6,7 +6,7 @@ func Abort(a, b, c, d, e int) int {
 	for i := 1; i < limit; i++ {
 		for j := 1; j < limit; j++ {
 			if arr[j-1] > arr[j] {
-				arr[i], arr[i-1] = arr[i-1], arr[i]
+				arr[j-1], arr[j-1] = arr[j-1], arr[j]
 			}
 		}
 		limit--
