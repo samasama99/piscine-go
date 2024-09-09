45489a590659aaecd83cdf3559cc2c4db7e73aa9
orahmouni <oussama.rahmouni.1999@gmail.com>
2024-09-09 16:43:10 +0100

    shoppingsummarycounter.go

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -2,8 +2,16 @@ package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
 	freq := make(map[string]int)
+	count := 0
 	for _, s := range Split(str, " ") {
 		freq[s]++
+		count++
 	}
+	for _, c := range str {
+		if c == ' ' {
+			count--
+		}
+	}
+	freq[""] = count
 	return freq
 }
